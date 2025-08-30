8d590a7d2673bce27aaee64107d05a0fb8d39e4e
L A Romeo <ukasyahrz@outlook.com>
2025-08-31 05:11:56 +0700

    refactor(StyledRenderer): remove unused style prop and simplify component

--- src/components/styled-renderer.client.ts
+++ src/components/styled-renderer.client.ts
@@ -5,12 +5,11 @@ import { type CSSObject, jsx } from '@emotion/react'
 
 interface StyledRendererProps {
   element: keyof JSX.IntrinsicElements
-  style: CSSObject
   children: React.ReactNode
   css?: CSSObject
   [key: string]: any
 }
 
-export default function StyledRenderer({ element, style = {}, css = {}, children, ...props }: StyledRendererProps) {
-  return jsx(element, { ...props, css: { ...style, ...css } }, children)
+export default function StyledRenderer({ element, css = {}, children, ...props }: StyledRendererProps) {
+  return jsx(element, { ...props, css }, children)
 }
