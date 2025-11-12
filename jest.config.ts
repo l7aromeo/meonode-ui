a3213eb5b91a55364cb4f5362005bc2a46934de5
L A Romeo <ukasyahrz@outlook.com>
2025-11-12 09:30:48 +0700

    chore: rename jest.config.mjs to jest.config.ts and update configuration for TypeScript support

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  moduleNameMapper: {
+    '^@src/(.*)\\.js$': '<rootDir>/src/$1.ts',
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: './tsconfig.json',
+      },
+    ],
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  injectGlobals: true,
+}
+
+export default config
